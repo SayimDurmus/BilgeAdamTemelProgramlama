5d76254b229ce6bbff32838fb68f97432cc1d364
durmuş sayım <durmus.sayim@gmail.com>
2023-01-26 02:28:22 +0300

    Hello GitHub

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //byte a = 255; // byte 0-255 arası değer alır
+            //sbyte b= -128; // sbyte -128 ve 127 arası değer alır
+
+            //ushort c = ushort.MaxValue;
+
+            //Console.WriteLine(a);
+
+            //float sayi = 556;
+            //Console.WriteLine(sayi);
+
+            //***************************************
+
+
+            //char a = 'd';
+            //string veri = "Merhaba Dünya";
+            //veri = veri + "a";
+            //Console.WriteLine(veri);
+
+            //while (true)
+            //{
+            //    veri = veri + "a"; //Döngü değişkenin içine sürekli bi  a karakteri ekliyo (Nereye kadar string bir ifadede?) RAM in max değerine erişene kadar ilgili değişkene eklemeye devam eder
+
+            //}
+
+            //**********************************************
+
+            //string text = "25";
+            //float sayi = float.Parse(text);
+            //int sayi2 = Convert.ToInt32(text);
+            //short sayi3 = Convert.ToInt16(text);
+            //long sayi4=Convert.ToInt64(text);
+
+            //Console.WriteLine(sizeof(int));
+            //Console.WriteLine(sizeof(double));
+            //Console.WriteLine(sizeof(decimal));
+
+            //********************************************
+
+            char a = 'a';
+            int veri = a;  // her bir karakterin ASCII kodu karsılığı vardır, bu durumda sayısal karşılığını yazar
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
